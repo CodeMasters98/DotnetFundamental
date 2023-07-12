066194c0d443acb78df0361405ecdba67bd3dc6e
Parham Darvishi <darvishiparham14@gmail.com>
2023-07-13 01:58:01 +0330

    Add New Projects

---
+++ 5.TypeConversionInCSharp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DotnetFundamental
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numInt = 500;
+
+            // get type of numInt
+            var n = numInt.GetType();
+
+            // Implicit Conversion
+            double numDouble = numInt;
+
+            // get type of numDouble
+            var n1 = numDouble.GetType();
+
+            // Value before conversion
+            Console.WriteLine("numInt value: " + numInt);
+            Console.WriteLine("numInt Type: " + n);
+
+            // Value after conversion
+            Console.WriteLine("numDouble value: " + numDouble);
+            Console.WriteLine("numDouble Type: " + n1);
+            Console.WriteLine("*************************************************");
+
+            string numberInString = "22";
+            Console.WriteLine("number in string: " + numberInString);
+            int number = int.Parse(numberInString);
+            var n3 = number.GetType();
+            Console.WriteLine("number in int: " + number);
+            Console.WriteLine("number in Type: " + n3);
+
+            Console.ReadLine();
+        }
+    }
+
+
+}
