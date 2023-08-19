a9be6110004e3927c2ff5e529f050845a7a6219d
darvishiparham14@gmail.com <darvishiparham14@gmail.com>
2023-08-19 15:46:30 +0330

    Add Project to sln +  Add Method Project

---
+++ 8.MethodInCsharp/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        ShowMessage();
+        ShowDevider();
+        ShowCustomMessage("This is message from out!");
+        ShowDevider();
+        AddNumbers(5, 10);
+        ShowDevider();
+        int result = AddNumbers(5, 10);
+        ShowDevider();
+    }
+
+    static void ShowMessage()
+    {
+        Console.WriteLine("Hi!!!");
+    }
+
+    static void ShowDevider()
+    {
+        Console.WriteLine("***************************************");
+    }
+
+    static void ShowCustomMessage(string message)
+    {
+        Console.WriteLine($"Hi!!! {message}");
+    }
+
+    static int AddNumbers(int number1 , int number2)
+    {
+        int result = number1 + number2;
+        return result;
+    }
+}
