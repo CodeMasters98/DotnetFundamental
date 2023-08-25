52591a60e48a8715d2398321180a00d7b2b5d92e
Parham darvishi <darvishiparham14@gmail.com>
2023-08-25 15:52:30 +0330

    Changes

---
+++ 100.Project-CleanData/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DotnetFundamental
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your cellphone:");
+            string cellphone = Console.ReadLine();
+            if (cellphone.Length == 10)
+            {
+                cellphone = "0" + cellphone;
+                Console.WriteLine("Add 0 if length is not enough!, Result: " + cellphone);
+            }
+            
+            if (cellphone.StartsWith("+98"))
+            {
+                cellphone = cellphone.Replace("+98", "0");
+                Console.WriteLine("Replace +98 with 0 !, Result: " + cellphone);
+            }
+
+            
+        }
+    }
+
+
+}
---
+++ Project-OddEven/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DotnetFundamental
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your Number:");
+            string numberStr = Console.ReadLine();
+            int number = int.Parse(numberStr);
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The number is even.");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd.");
+            }
+        }
+    }
+
+}
