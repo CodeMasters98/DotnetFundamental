0be7a22cb7598579d0406cafc673f330677e6008
Parham darvishi <darvishiparham14@gmail.com>
2023-09-01 16:12:49 +0330

    Change On Branch

--- BranchingInCSharp/Program.cs
+++ BranchingInCSharp/Program.cs
@@ -25,6 +25,32 @@ namespace DotnetFundamental
             {
                 Console.WriteLine("both are Equals");
             }
+
+            int day = 4;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+            }
         }
     }
 
